55f76c52e8547eb9431ad9f31b9ff2d7e20d797d
Ahmedlebshten <ahmedlebshtenlebshten@gmail.com>
2026-02-10 02:32:57 +0200

    edit app/Config/Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -24,10 +24,15 @@ class Database
         $dsn = "mysql:host=$host;port=$port;dbname=$db;charset=utf8mb4";
 
         try {
-            self::$conn = new PDO($dsn, $user, $pass, [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            ]);
+            self::$conn = new PDO(
+                $dsn,
+                $user,
+                $pass,
+                [
+                    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+                ]
+            );
         } catch (PDOException $e) {
             die("Database connection failed: " . $e->getMessage());
         }
