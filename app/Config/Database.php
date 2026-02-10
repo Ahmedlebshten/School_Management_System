b09881528f98ec62395a50ee8c341031a3db4288
Ahmedlebshten <ahmedlebshtenlebshten@gmail.com>
2026-02-10 02:10:37 +0200

    edit app/Config/Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -27,15 +27,15 @@ class Database
         $dbName    = $_ENV['DB_NAME'] ?? getenv('DB_NAME');
         $dbUser    = $_ENV['DB_USER'] ?? getenv('DB_USER');
         $dbPass    = $_ENV['DB_PASS'] ?? getenv('DB_PASS');
-        $dbCharset = $_ENV['DB_CHARSET'] ?? getenv('DB_CHARSET') ?? 'utf8mb4';
+        $dsn = "mysql:host={$dbHost};dbname={$dbName};charset=utf8mb4";
 
-        $dsn = "mysql:host={$dbHost};dbname={$dbName};charset={$dbCharset}";
 
         $options = [
-            PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES {$dbCharset}",
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ];
+           PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4",
+           PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+           PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+];
+
 
         try {
             self::$conn = new PDO($dsn, $dbUser, $dbPass, $options);
